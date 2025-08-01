cee5425d1aec045010fb7261f0d3fdd4310b4e8e
Devang <devang.patani0806@gmail.com>
2025-08-01 08:32:21 +0530

    Add CSS to To-Do App

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -66,19 +66,21 @@ class App extends Component {
     const { inputValue, listContainer } = this.state;
     return(
       <div className = "App" >
-        <h1>To-Do List</h1>
+        <h1>My To-Do List 📝</h1>
         <div className="input-container">
-          <input type="text" name="task" placeholder='Enter a task...' value={inputValue} onChange={this.handleInput} />
-          <button type="button" onClick={this.addTask}>Add Task</button>
+          <input className='input' type="text" name="task" placeholder='Enter a task...' value={inputValue} onChange={this.handleInput} />
+          <button className='button' type="button" onClick={this.addTask}>Add Task</button>
         </div>
         <ul className='task-list'>
           {(listContainer.length === 0) ? (
-            <p>No task yet. Add one above!</p>
+            <p>No tasks yet! Add some above. ⬆️</p>
           ) : (
               listContainer.map((task) => (
                 <li key={task.id}>
-                  <input type="checkbox" checked={task.completed} onChange={() => this.toggleTask(task.id)} />
-                  <span>{task.text}</span>
+                  <div>
+                    <input type="checkbox" checked={task.completed} onChange={() => this.toggleTask(task.id)} />
+                    <span style={{ textDecoration: task.completed ? 'line-through' : 'none', color: task.completed ? 'gray' : 'black' }}>{task.text}</span>
+                  </div>
                   <button type="button" onClick={() => this.deleteTask(task.id)}>Delete</button>
                 </li>
               ))
